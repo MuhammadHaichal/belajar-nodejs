88bf439e988dc46fb941f713aafa44aa5b9917d2
localhostkali06@gmail.com <localhostkali06@gmail.com>
2023-12-29 08:01:45 +0700

    perbaikan search button

--- src/route/app.js
+++ src/route/app.js
@@ -10,29 +10,27 @@ const bodyjson = bodyParser.json()
 
 
 
-
-
 // select data
 // ===========
+const title = 'data karyawan';
 router.get('/', (req, res) => {
     const sql = 'SELECT * FROM karyawan'
     db.query(sql, (err, result) => {
-
         const parse = JSON.parse(JSON.stringify(result))
-        res.render('index', { 'hasil': parse, 'title': 'data karyawan' })
-
+        res.render('index', {
+            'hasil': parse,
+            'title': title,
+            'errDataNotFound': ''
+        })
     })
 })
-
 router.post('/', urlencoded, (req, res) => {
-    const sql = `SELECT * FROM karyawan WHERE nama = '${req.body.getDataUser}'`
+    const sql = `SELECT * FROM karyawan WHERE nama LIKE '${req.body.getDataUser}%' `
     db.query(sql, (err, result) => {
-        if (err == null) {
-            console.log('data tidak ditemukan');
-        }
-        else {
-            res.render('index', { 'hasil': result, 'title': 'data karyawan' })
-        }
+        res.render('index', {
+            'hasil': result,
+            'title': title,
+        })
     })
 })
 
@@ -40,49 +38,44 @@ router.post('/', urlencoded, (req, res) => {
 
 // insert data 
 // ===========
+let insertTitle = 'TAMBAH KARYAWAN';
 router.get('/add', (req, res) => {
-    let title = 'TAMBAH KARYAWAN';
     res.render('tambahData', {
-        'title': title,
+        'title': insertTitle,
         'errMsgNama': '',
         'errMsgUmur': '',
         'errMsgNamaInput': '',
         'errMsgUmurInput': '',
     })
 })
-
-
 router.post('/add', urlencoded, (req, res) => {
-    let title = 'TAMBAH KARYAWAN';
     const getName = req.body.nama;
     const getUmur = req.body.umur;
 
     if (validator.isEmpty(getName)) {
         res.render('tambahData', {
-            'title': title,
+            'title': insertTitle,
             'errMsgNama': 'd-block',
             'errMsgNamaInput': 'is-invalid',
             'errMsgUmur': '',
             'errMsgUmurInput': '',
         })
     }
-
     else if (validator.isEmpty(getUmur)) {
         res.render('tambahData', {
-            'title': title,
+            'title': insertTitle,
             'errMsgUmur': 'd-block',
             'errMsgUmurInput': 'is-invalid',
             'errMsgNama': '',
             'errMsgNamaInput': '',
         })
     }
-
     else {
         const sql = `INSERT INTO karyawan(nama, umur) VALUES ( '${req.body.nama}', ${req.body.umur})`
         db.query(sql, (err, result) => {
             if (err) {
                 res.render('tambahData', {
-                    'title': title,
+                    'title': insertTitle,
                     'errMsg': err
                 })
             }
@@ -99,7 +92,6 @@ router.post('/add', urlencoded, (req, res) => {
 // ============
 router.get('/delete/:id', (req, res) => {
     const sql = `DELETE FROM karyawan WHERE id = ${req.params.id}`
-
     db.query(sql, (err, result) => {
         if (err) {
             console.log(err);
@@ -109,6 +101,7 @@ router.get('/delete/:id', (req, res) => {
         }
     })
 })
+
 
 
 module.exports = router;
